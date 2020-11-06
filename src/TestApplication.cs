4bd2ff5ac7251d9935117ff723a308d21a167c6d
Stephan van Stekelenburg <stephan@hayman.io>
2020-11-06 12:03:01 +0100

    initial commit

---
+++ src/TestApplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loupedeck;
+
+namespace TestPlugin
+{
+    public class TestApplication : ClientApplication
+    {
+        protected override string GetProcessName() => "Notepad";
+
+        protected override string GetBundleName() => string.Empty;
+    }
+}
---
+++ src/TestPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Loupedeck;
+
+namespace TestPlugin
+{
+    public class TestPlugin : Plugin
+    {
+        public override void ApplyAdjustment(string adjustmentName, string parameter, int diff)
+        {
+        }
+
+        public override void RunCommand(string commandName, string parameter)
+        {
+        }
+    }
+}
